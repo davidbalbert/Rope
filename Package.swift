2d01eedf9ddf8e0c9b00738f78e586966dec09c5
David Albert <davidbalbert@gmail.com>
2023-06-21 12:55:32 -0400

    Remove SimpleRope

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SimpleRope",
-            targets: ["SimpleRope"]),
-        .library(
             name: "Rope",
             targets: ["Rope"]),
     ],
@@ -21,12 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SimpleRope",
-            dependencies: []),
-        .testTarget(
-            name: "SimpleRopeTests",
-            dependencies: ["SimpleRope"]),
         .target(
             name: "Rope",
             dependencies: [],
