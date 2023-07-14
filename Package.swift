c774f5122e550bce660fd2c49eedb8c65b140584
David Albert <davidbalbert@gmail.com>
2023-07-14 15:49:40 -0400

    Remove RopePerformanceTests target

--- Package.swift
+++ Package.swift
@@ -23,13 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Rope",
-            dependencies: [],
-            swiftSettings: [.unsafeFlags(["-enable-testing"])]),
+            dependencies: []),
         .testTarget(
             name: "RopeTests",
             dependencies: ["Rope"]),
-        .testTarget(
-            name: "RopePerformanceTests",
-            dependencies: ["Rope"])
     ]
 )
